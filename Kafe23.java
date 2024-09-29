1fde8b4cb8f4978a3956bfd0193d53abd0211c99
Abbiyi <3dy5upriono@gmail.com>
2024-09-29 12:49:37 +0700

    Kafe 23

--- Kafe23.java
+++ Kafe23.java
@@ -50,7 +50,7 @@ public class Kafe23 {
         double nominalBayar = totalHarga - (diskon * totalHarga);
 
         System.out.println("Item pembelian: " + jumlah + " " + menu + " dengan ukuran cup " + ukuranCup);
-        System.out.println("Nominal bayar: " + nominalBayar);
+        System.out.println("Nominal bayar: " + nominalBayar); 
     }
 }
     
