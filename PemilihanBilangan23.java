dacf557a20d3302ab4b5257aab8e91a0263320c2
Abbiyi <3dy5upriono@gmail.com>
2024-09-29 20:19:12 +0700

    Pemilihan Bilangan 23

--- PemilihanBilangan23.java
+++ PemilihanBilangan23.java
@@ -14,11 +14,5 @@ public class PemilihanBilangan23 {
         // Menggunakan operator ternary
         String hasil = (angka % 2 == 0) ? "termasuk bilangan genap" : "termasuk bilangan ganjil";
         System.out.println("Angka " + angka + " " + hasil); 
-
-
-        else if (nilaiAkhir < 39) {
-            nilaiHuruf = "E";
-            nilaiSetara = 0.0;
-            kualifikasi = "Gagal" ; 
     }
 }
