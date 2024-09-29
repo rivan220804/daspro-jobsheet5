fbd0a6a7ad90c7de66effb7019f70e172eb02aa0
Abbiyi <3dy5upriono@gmail.com>
2024-09-29 12:01:57 +0700

    PemilihanBilangan23

--- PemilihanBilangan23.java
+++ PemilihanBilangan23.java
@@ -8,17 +8,11 @@ public class PemilihanBilangan23 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
 
-        System.out.println("Masukkan sebuah angaka: ");
+        System.out.println("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
 
-        if (angka % 2 == 0) 
-        {
-            System.out.println("Angka " + angka + "termasuk bilangan genap");
-        }
-        else
-        {
-            System.out.println("Angka " + angka + "termasuk bilangan ganjil"); 
-        }
-
+        // Menggunakan operator ternary
+        String hasil = (angka % 2 == 0) ? "termasuk bilangan genap" : "termasuk bilangan ganjil";
+        System.out.println("Angka " + angka + " " + hasil); 
     }
 }
